a8a2b30c5748f45d84a5da39e650313096fb5d16
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-02-27 20:24:36 +0700

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,7 @@ module.exports = {
   plugins: [
     'postcss-preset-env',
     purgecss({
-      content: ['./src/**/*.js'],
-      // Treat every word in the bundle as a CSS selector
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      content: ['./**/*.html'],
     }),
   ],
 };
